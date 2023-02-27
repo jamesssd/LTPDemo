54451ec88bde398ce2c0fe318c5b6ca174317e78
James Dizon <dizonjerlin@gmail.com>
2023-02-27 09:39:24 -0800

    Initial commit

---
+++ src/main/java/com/example/LTPDemo/LtpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.LTPDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtpDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LTPDemo/LtpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LTPDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
